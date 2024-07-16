f8d6e72e06dc04af98b770ee08c8729224146a54
Jaime Aquino Martinez <jhonesbortoni@gmail.com>
2024-07-16 16:21:17 -0500

    xd nomas

---
+++ Node.js/ExpressJS/Controllers/movies.js
@@ -0,0 +1,66 @@
+//Importacion de los Modelos
+import { movieModel } from "../Models/movie";
+
+// Importación de los métodos de validación de ZOD
+import { validateMovie, validatePartialMovie } from "./Schemas/Schema.js";
+
+export class MovieController {
+    static async getALL(req, res) {
+        const { genre } = req.query;
+        const movies = await movieModel.getALL({ genre });
+
+        //Que es lo que Renderiza
+        res.json(movies);
+    }
+
+    static async getById(req, res) {
+        const { id } = req.params;
+        const movie = await movieModel.getByID({ id });
+        if (movie) return res.json(movie);
+
+        //Que es lo que Renderiza
+        res.status(404).json({ message: "Pelicula no encontrada" });
+    }
+
+    static async create(req, res) {
+        const result = validateMovie(req.body);
+
+        if (result.error) {
+            return res
+                .status(400)
+                .json({ error: JSON.parse(result.error.message) });
+        }
+
+        const newMovie = await movieModel.create({ input: result.data });
+        res.status(201).json(newMovie);
+    }
+
+    static async delete(req, res) {
+        const { id } = req.params;
+        const movieIndex = await movieModel.delete({ id });
+
+        if (!movieIndex)
+            return res.status(404).json({ message: "Pelicula no encontrada" });
+
+        return res.json({ message: "Movie Deleted" });
+    }
+
+    static async update(req, res) {
+        const result = validatePartialMovie(req.body);
+
+        if (result.error) {
+            return res
+                .status(400)
+                .json({ error: JSON.parse(result.error.message) });
+        }
+
+        const { id } = req.params;
+        const updatedMovie = await movieModel.update({
+            id,
+            input: result.data,
+        });
+        movies[movieIndex] = updatedMovie;
+
+        return res.json(updatedMovie);
+    }
+}
--- Node.js/ExpressJS/Routes/movies.js
+++ Node.js/ExpressJS/Routes/movies.js
@@ -1,65 +1,17 @@
 //Importacion de las Dppendencias
 import { Router } from "express";
-
-// Importación de los métodos de validación de ZOD
-import { validateMovie, validatePartialMovie } from "./Schemas/Schema.js";
-
-// Importación del json de películas
-import { movieModel } from "../Models/movie.js";
+import { MovieController } from "../Controllers/movies.js";
 
 // Creacion del Router
 export const moviesRouter = new Router();
 
 // RUTAS
-moviesRouter.get("/", async (req, res) => {
-    const { genre } = req.query;
-    const movies = await movieModel.getALL({ genre });
-    res.json(movies);
-});
+moviesRouter.get("/", MovieController.getALL);
 
-moviesRouter.get("/:id", async (req, res) => {
-    const { id } = req.params;
-    const movie = await movieModel.getByID({ id });
-    if (movie) return res.json(movie);
+moviesRouter.get("/:id", MovieController.getById);
 
-    res.status(404).json({ message: "Pelicula no encontrada" });
-});
+moviesRouter.post("/", MovieController.create);
 
-moviesRouter.post("/", async (req, res) => {
-    const result = validateMovie(req.body);
+moviesRouter.delete("/:id", MovieController.delete);
 
-    if (result.error) {
-        return res
-            .status(400)
-            .json({ error: JSON.parse(result.error.message) });
-    }
-
-    const newMovie = await movieModel.create({ input: result.data });
-    res.status(201).json(newMovie);
-});
-
-moviesRouter.delete("/:id", async (req, res) => {
-    const { id } = req.params;
-    const movieIndex = await movieModel.delete({ id });
-
-    if (!movieIndex)
-        return res.status(404).json({ message: "Pelicula no encontrada" });
-
-    return res.json({ message: "Movie Deleted" });
-});
-
-moviesRouter.patch("/:id", async (req, res) => {
-    const result = validatePartialMovie(req.body);
-
-    if (result.error) {
-        return res
-            .status(400)
-            .json({ error: JSON.parse(result.error.message) });
-    }
-
-    const { id } = req.params;
-    const updatedMovie = await movieModel.update({ id, input: result.data });
-    movies[movieIndex] = updatedMovie;
-
-    return res.json(updatedMovie);
-});
+moviesRouter.patch("/:id", MovieController.update);
