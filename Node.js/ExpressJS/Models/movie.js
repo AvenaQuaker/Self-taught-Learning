9af3ef01f180ddf3908c2190ca342bc4fd4830b2
Jaime Aquino Martinez <jhonesbortoni@gmail.com>
2024-07-15 21:41:19 -0500

    Cambios

---
+++ Node.js/ExpressJS/Models/movie.js
@@ -0,0 +1,51 @@
+// Importacion de dependencias
+import { randomUUID } from 'crypto';
+
+// Importación del json de películas
+import movies from "../movies.json" with { type: "json" };
+
+// Clase Modelo Movie
+export class movieModel {
+    static async getALL({ genre }){
+        if (genre) {
+            return movies.filter((movie) =>
+                movie.genre.some((g) => g.tolowercase() === genre.tolowercase())
+            );
+        }
+
+        return movies;
+    }
+
+    static async getByID({id}){
+        const movie = movies.find(movie => movie.id === id);
+        return movie;
+    }
+
+    static async create({input}){
+        const newMovie = {
+            id: randomUUID(),
+            ...input
+        };
+        movies.push(newMovie);
+        return newMovie;
+    }
+
+    static async delete({id}){
+        const movieIndex = movies.findIndex((movie) => movie.id === id);
+        if(movieIndex === -1) return false
+
+        movies.splice(movieIndex, 1);
+        return true;
+    }
+
+    static async update({id, input}){
+        const movieIndex = movies.findIndex((movie) => movie.id === id);
+        if(movieIndex === -1) return false
+
+        movies[movieIndex] = {
+            ...movies[movieIndex],
+            ...input};
+
+        return movies[movieIndex];
+    } 
+}
--- Node.js/ExpressJS/Routes/movies.js
+++ Node.js/ExpressJS/Routes/movies.js
@@ -1,37 +1,31 @@
 //Importacion de las Dppendencias
 import { Router } from "express";
-import crypto from "crypto";
 
 // Importación de los métodos de validación de ZOD
 import { validateMovie, validatePartialMovie } from "./Schemas/Schema.js";
 
 // Importación del json de películas
-import movies from "./movies.json" with { type: "json" };
+import { movieModel } from "../Models/movie.js";
 
 // Creacion del Router
 export const moviesRouter = new Router();
 
 // RUTAS
-moviesRouter.get("/", (req, res) => {
+moviesRouter.get("/", async (req, res) => {
     const { genre } = req.query;
-    if (genre) {
-        const filteredMovies = movies.filter((movie) =>
-            movie.genre.some((g) => g.toLowerCase() === genre.toLowerCase())
-        );
-        return res.json(filteredMovies);
-    }
+    const movies = await movieModel.getALL({ genre });
     res.json(movies);
 });
 
-moviesRouter.get('/:id', (req, res) => {
+moviesRouter.get("/:id", async (req, res) => {
     const { id } = req.params;
-    const movie = movies.find((movie) => movie.id === id);
+    const movie = await movieModel.getByID({ id });
     if (movie) return res.json(movie);
 
     res.status(404).json({ message: "Pelicula no encontrada" });
-})
+});
 
-moviesRouter.post('/', (req, res) => {
+moviesRouter.post("/", async (req, res) => {
     const result = validateMovie(req.body);
 
     if (result.error) {
@@ -40,28 +34,21 @@ moviesRouter.post('/', (req, res) => {
             .json({ error: JSON.parse(result.error.message) });
     }
 
-    const newMovie = {
-        id: crypto.randomUUID(),
-        ...result.data,
-    };
-    movies.push(newMovie);
+    const newMovie = await movieModel.create({ input: result.data });
     res.status(201).json(newMovie);
-})
+});
 
-moviesRouter.delete("/:id", (req, res) => {
+moviesRouter.delete("/:id", async (req, res) => {
     const { id } = req.params;
-    const movieIndex = movies.findIndex((movie) => movie.id === id);
+    const movieIndex = await movieModel.delete({ id });
 
-    if (movieIndex === -1) {
+    if (!movieIndex)
         return res.status(404).json({ message: "Pelicula no encontrada" });
-    }
-
-    movies.splice(movieIndex, 1);
 
     return res.json({ message: "Movie Deleted" });
 });
 
-moviesRouter.patch("/:id", (req, res) => {
+moviesRouter.patch("/:id", async (req, res) => {
     const result = validatePartialMovie(req.body);
 
     if (result.error) {
@@ -71,18 +58,8 @@ moviesRouter.patch("/:id", (req, res) => {
     }
 
     const { id } = req.params;
-    const movieIndex = movies.findIndex((movie) => movie.id === id);
+    const updatedMovie = await movieModel.update({ id, input: result.data });
+    movies[movieIndex] = updatedMovie;
 
-    if (movieIndex === -1) {
-        return res.status(404).json({ message: "Pelicula no encontrada" });
-    }
-
-    const updateMovie = {
-        ...movies[movieIndex],
-        ...result.data,
-    };
-
-    movies[movieIndex] = updateMovie;
-
-    return res.json(updateMovie);
+    return res.json(updatedMovie);
 });
